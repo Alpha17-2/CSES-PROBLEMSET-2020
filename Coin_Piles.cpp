ebf414c7c169512cda57708bbd1de1ef1a03715f
Your Name <you@example.com>
2020-10-21 17:50:58 +0530

    New Problem Added

--- Coin_Piles.cpp
+++ Coin_Piles.cpp
@@ -1,5 +1,5 @@
 /*  Author : Alpha_The_Coder
-    Created on 19/10/2020      */
+    Created on 21/10/2020      */
 #include <bits/stdc++.h>
 using namespace std;
 #define iamalpha ios_base::sync_with_stdio(false);cin.tie(NULL);
@@ -8,36 +8,23 @@ using namespace std;
 #define lli long long int
 #define even(n) n%2==0?true:false
 void solve();
+
 int main()
 {
     iamalpha;
     int t=1;
-   // scanf("%d",&t);
+    scanf("%d",&t);
     while(t--) {
         solve();
     }
     return 0;
 }
 void solve()
-{    
-    int n;
-    cin>>n;
-    if(n==2)
-   {
-       int first,second;
-       cin>>first>>second;
-       cout<<abs(first-second);
-       return;
-   }
-    lli sum=0;
-    lli val;
-    loop(0,n) {
-        cin>>val;
-        sum+=val;
-    }
-    if(even(sum))
-    cout<<0;
-    else
-    cout<<sum%2;  
+{  
+ 
+ lli a,b;
+ cin>>a>>b;
+ lli sum=a+b;
+ if( a<=b*2 && (sum)%3 == 0 && b<=a*2) cout<<"YES\n";
+ else cout<<"NO\n";
 }
-
---
+++ Trailing_Zeroes.cpp
@@ -0,0 +1,28 @@
+/*  Author : Alpha_The_Coder
+    Created on 21/10/2020      */
+#include <bits/stdc++.h>
+using namespace std;
+#define iamalpha ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define loop(a,n) for(int i=a;i<n;i++)
+#define nloop(a,n) for(int j=a;j<n;j++)
+#define lli long long int
+#define even(n) n%2==0?true:false
+void solve();
+int main()
+{
+    iamalpha;
+    int t=1;
+   // scanf("%d",&t);
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+void solve()
+{    
+   int n; cin>>n;
+   int ans=0;
+   for(int i=5;(n/i)>=1;i*=5)
+   ans+=(n/i);
+   cout<<ans;
+}
