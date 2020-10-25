fc704283afbf0a334501f16738c84cc3534cff80
Your Name <you@example.com>
2020-10-25 11:59:14 +0530

    Updated.

--- Sum_of_Four_values.cpp
+++ Sum_of_Four_values.cpp
@@ -1,5 +1,5 @@
 /*  Author : Alpha_The_Coder
-    Created on 23/10/2020      */
+    Created on 25/10/2020      */
 #include <bits/stdc++.h>
 using namespace std;
 #define iamalpha ios_base::sync_with_stdio(false);cin.tie(NULL);
@@ -24,36 +24,49 @@ void solve()
     cin>>n;
     lli target;
     cin>>target;
-    pair<lli,int>a[n];
+    vector<lli>a(n);
     loop(0,n) {
-        lli val;
-        cin>>val;
-        a[i].first=val;
-        a[i].second=i;
+        cin>>a[i];
     }
-    sort(a,a+n);
-    for(int i=0;i<n;++i)
+    int total= (n * (n-1) )/2;
+    pair<lli,pair<int,int>>p[total];
+    int current_index=0;
+    for(int i=0;i<n;i++)
     {
-        for(int j=i+1;j<n;++j)
+        for(int  j=i+1;j<n;j++)
         {
-            for (int k = j+1; k < n; ++k)
-            {
-                lli current_sum=a[i].first+a[j].first+a[k].first;
-                int l=n-1;
-                while (l>k && current_sum+a[l].first>target)
-                {
-                    --l;
-                }
-                if(l>k && current_sum+a[l].first==target)
-                {
-                    cout<<a[i].second+1<<" "<<a[j].second+1<<" "<<a[k].second+1<<" "<<a[l].second+1;
-                    return;
-                }
-            }
-            
-            
+            lli sum=a[i]+a[j];
+            p[current_index].first=sum;
+            p[current_index].second.first=i;
+            p[current_index].second.second=j;
+            current_index++;
         }
     }
-    cout<<"IMPOSSIBLE";
-       
+    sort(p,p+total);
+    for(int i=0;i<total;i++)
+    {
+        if(p[i].first>=target)
+        {
+            cout<<"IMPOSSIBLE"; return;
+        }
+        int j=total-1;
+        while (j>i && p[i].first+p[j].first>target)
+        {
+            j--;
+        }
+        if(j>i && p[i].first+p[j].first==target)
+        {
+           int a,b,c,d;
+           a=p[i].second.first;
+           b=p[i].second.second;
+           c=p[j].second.first;
+           d=p[j].second.second;
+           if(a!=c && a!=d && b!=c && b!=d)
+           {
+               cout<<a+1<<" "<<b+1<<" "<<c+1<<" "<<d+1;
+               return;
+           }
+        }
+    }
+    cout<<"IMPOSSIBLE";  
 }
